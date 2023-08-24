300faf6c8639caca59658c33c113be1de034518e
Vice <vicerust@gmail.com>
2023-08-24 18:57:02 -0400

    fix ender dragon and create contraptions getting culled

--- src/main/java/vice/rubidium_extras/mixins/EntityDistance/MaxDistanceEntity.java
+++ src/main/java/vice/rubidium_extras/mixins/EntityDistance/MaxDistanceEntity.java
@@ -3,6 +3,8 @@ package vice.rubidium_extras.mixins.EntityDistance;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.monster.Ghast;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,6 +19,16 @@ public class MaxDistanceEntity
     public <E extends Entity> void shouldDoRender(E entity, Frustum clippingHelper, double cameraX, double cameraY, double cameraZ, CallbackInfoReturnable<Boolean> cir)
     {
         if (!MagnesiumExtrasConfig.enableDistanceChecks.get())
+            return;
+
+        if (entity instanceof EnderDragon)
+            return;
+
+        if (entity instanceof Ghast)
+            return;
+
+        var name = entity.getClass().getName();
+        if (name != null && name.startsWith("com.simibubi.create.content.contraptions"))
             return;
 
         if (!DistanceUtility.isEntityWithinDistance(
