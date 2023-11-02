04a52daa1713119821d976585ce94afcbbac0c2a
SrRapero720 <SrRapero720@hotmail.com>
2023-11-01 23:49:27 -0600

    compile... damn you

--- src/main/java/me/srrapero720/embeddium_extras/mixins/fog/MixinBackgroundRenderer.java
+++ src/main/java/me/srrapero720/embeddium_extras/mixins/fog/MixinBackgroundRenderer.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public class MixinBackgroundRenderer {
 //    @ModifyArg(method = "setupFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V"), index = 0)
     private static float modifySetShaderFogStart(float original) {
-        return original * ((float) EmbeddiumExtrasConfig.fogStart / 100);
+        return 0;
+//        return original * ((float) EmbeddiumExtrasConfig.fogStart / 100);
     }
 }
