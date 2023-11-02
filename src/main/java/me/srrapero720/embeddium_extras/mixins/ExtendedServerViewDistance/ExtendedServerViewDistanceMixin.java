a3654bca5bb3e5b3ee623a439e0f390dd9bf7194
SrRapero720 <SrRapero720@hotmail.com>
2023-11-01 20:30:22 -0600

    minor changes again

--- src/main/java/me/srrapero720/embeddium_extras/mixins/ExtendedServerViewDistance/ExtendedServerViewDistanceMixin.java
+++ src/main/java/me/srrapero720/embeddium_extras/mixins/ExtendedServerViewDistance/ExtendedServerViewDistanceMixin.java
@@ -12,9 +12,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
-@Mixin({ClientPacketListener.class})
+@Mixin(ClientPacketListener.class)
 public class ExtendedServerViewDistanceMixin {
-    @Inject(method = {"handleForgetLevelChunk"}, at = {@At("HEAD")}, cancellable = true)
+    @Inject(method = "handleForgetLevelChunk", at = @At("HEAD"), cancellable = true)
     private void onUnload(ClientboundForgetLevelChunkPacket packet, CallbackInfo ci) {
         ci.cancel();
     }
