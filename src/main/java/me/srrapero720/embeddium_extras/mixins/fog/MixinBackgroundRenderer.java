afaa4c768f0d51d674e81cdaea328ab196eda5ba
SrRapero720 <SrRapero720@hotmail.com>
2023-11-01 23:48:59 -0600

    experimental impl of sodium extras

---
+++ src/main/java/me/srrapero720/embeddium_extras/mixins/fog/MixinBackgroundRenderer.java
@@ -0,0 +1,15 @@
+package me.srrapero720.embeddium_extras.mixins.fog;
+
+import me.srrapero720.embeddium_extras.config.EmbeddiumExtrasConfig;
+import net.minecraft.client.renderer.FogRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+//@Mixin(FogRenderer.class)
+public class MixinBackgroundRenderer {
+//    @ModifyArg(method = "setupFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V"), index = 0)
+    private static float modifySetShaderFogStart(float original) {
+        return original * ((float) EmbeddiumExtrasConfig.fogStart / 100);
+    }
+}
