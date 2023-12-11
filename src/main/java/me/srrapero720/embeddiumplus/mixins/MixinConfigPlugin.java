9b99329dd66ebf014eafebb917cccc73a18ef2a5
SrRapero720 <SrRapero720@hotmail.com>
2023-12-11 03:57:02 -0600

    mixin changes around JEI

--- src/main/java/me/srrapero720/embeddiumplus/mixins/MixinConfigPlugin.java
+++ src/main/java/me/srrapero720/embeddiumplus/mixins/MixinConfigPlugin.java
@@ -1,8 +1,9 @@
 package me.srrapero720.embeddiumplus.mixins;
 
-import me.srrapero720.embeddiumplus.EmbeddiumPlus;
 import net.minecraftforge.fml.loading.FMLEnvironment;
-import net.minecraftforge.fml.loading.LoadingModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -10,9 +11,11 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import java.util.List;
 import java.util.Set;
 
+import static me.srrapero720.embeddiumplus.EmbeddiumPlus.LOGGER;
+
 public class MixinConfigPlugin implements IMixinConfigPlugin {
-    private boolean hasFarSight() { return LoadingModList.get().getModFileById("farsight_view") != null; }
-    private boolean hasBobby() { return LoadingModList.get().getModFileById("bobby") != null; }
+    private static final Marker IT = MarkerManager.getMarker("MixinPlugin");
+    private static boolean isPresent(String modid) { return FMLLoader.getLoadingModList().getModFileById(modid) != null; }
 
     @Override
     public void onLoad(String mixinPackage) {
@@ -24,14 +27,16 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
     }
 
     @Override
-    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        if (!FMLEnvironment.dist.isClient())
-            return false;
+    public boolean shouldApplyMixin(String target, String mixin) {
+        if (FMLEnvironment.dist.isDedicatedServer()) return false;
 
 
-        if (mixinClassName.contains("ExtendedServerViewDistance") && (hasFarSight() || hasBobby())) {
-            EmbeddiumPlus.LOGGER.info("Disabling ExtendedServerViewDistance mixin due to Farsight/Bobby being installed.");
+        if (mixin.contains("ExtendedServerViewDistance") && (isPresent("farsight_view") || isPresent("bobby"))) {
+            LOGGER.warn(IT,"Disabling ExtendedServerViewDistance mixin due to Farsight/Bobby being installed.");
             return false;
+        }
+        if (mixin.contains("JeiOverlayMixin") && !isPresent("jei")) {
+            LOGGER.warn(IT, "Disabling JeiOverlay mixin due to JEI being NOT installed");
         }
 
         return true;
--- src/main/java/me/srrapero720/embeddiumplus/mixins/impl/jei/JeiOverlayMixin.java
+++ src/main/java/me/srrapero720/embeddiumplus/mixins/impl/jei/JeiOverlayMixin.java
@@ -1,12 +1,12 @@
 package me.srrapero720.embeddiumplus.mixins.impl.jei;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import me.srrapero720.embeddiumplus.config.EmbeddiumPlusConfig;
-import mezz.jei.api.gui.handlers.IGuiProperties;
 import mezz.jei.gui.elements.GuiIconToggleButton;
+import mezz.jei.gui.input.GuiTextFieldFilter;
 import mezz.jei.gui.overlay.IngredientListOverlay;
-import mezz.jei.input.GuiTextFieldFilter;
+import mezz.jei.gui.overlay.ScreenPropertiesCache;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,26 +15,26 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = IngredientListOverlay.class, remap = false)
-public class IngredientListOverlayMixin {
+public class JeiOverlayMixin {
     @Shadow @Final private GuiTextFieldFilter searchField;
     @Shadow @Final private GuiIconToggleButton configButton;
-    @Shadow private IGuiProperties guiProperties;
+    @Shadow @Final private ScreenPropertiesCache screenPropertiesCache;
 
-    @Inject(method = "drawScreen", at = @At(value = "INVOKE", target = "Lmezz/jei/gui/overlay/IngredientGridWithNavigation;draw(Lnet/minecraft/client/Minecraft;Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V"), cancellable = true)
-    public void render(Minecraft minecraft, PoseStack poseStack, int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
+    @Inject(method = "drawScreen", at = @At(value = "INVOKE", target = "Lmezz/jei/gui/overlay/IngredientGridWithNavigation;draw(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/gui/GuiGraphics;IIF)V"), cancellable = true)
+    public void render(Minecraft minecraft, GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
         if (!EmbeddiumPlusConfig.hideJEI.get()) return;
 
         String value = searchField.getValue();
         if (value.isEmpty()) {
-            if (guiProperties != null) {
-                configButton.draw(poseStack, mouseX, mouseY, partialTicks);
+            if (screenPropertiesCache.hasValidScreen()) {
+                configButton.draw(guiGraphics, mouseX, mouseY, partialTicks);
             }
             ci.cancel();
         }
     }
 
     @Inject(method = "drawTooltips", at = @At(value = "HEAD"), cancellable = true)
-    public void render(Minecraft minecraft, PoseStack poseStack, int mouseX, int mouseY, CallbackInfo ci) {
+    public void render(Minecraft minecraft, GuiGraphics guiGraphics, int mouseX, int mouseY, CallbackInfo ci) {
         if (!EmbeddiumPlusConfig.hideJEI.get()) return;
 
         String value = searchField.getValue();
